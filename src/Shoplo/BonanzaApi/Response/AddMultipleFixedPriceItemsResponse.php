9a2cd5f2c1315c2a5979cced712828ef0dc82f48
Andrei Susanu <andrei.susanu@gmail.com>
2019-04-30 21:39:17 +0300

    Adding support for addMultipleFixedPriceItems. WIP

--- src/Shoplo/BonanzaApi/Response/AddMultipleFixedPriceItemsResponse.php
+++ src/Shoplo/BonanzaApi/Response/AddMultipleFixedPriceItemsResponse.php
@@ -7,29 +7,8 @@ namespace Shoplo\BonanzaApi\Response;
 use JMS\Serializer\Annotation as Serializer;
 use Shoplo\BonanzaApi\Type\MultipleFixedPriceItemsResponseType;
 
-class AddMultipleFixedPriceItemsResponse
+class AddMultipleFixedPriceItemsResponse extends BaseResponse
 {
-//    /**
-//     * @var \DateTime
-//     *
-//     * @Serializer\Type("DateTime<Y-m-d\TH:i:s.u\Z>")
-//     */
-//    public $timestamp;
-
-    /**
-     * @var string
-     *
-     * @Serializer\Type("string")
-     */
-    public $version;
-
-    /**
-     * @var string
-     *
-     * @Serializer\Type("string")
-     */
-    public $ack;
-
     /**
      * @var MultipleFixedPriceItemsResponseType
      *
